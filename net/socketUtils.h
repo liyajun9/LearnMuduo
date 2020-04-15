9448353b9f718ebd19601743bc6d09decaa5e82d
Yajunli <liyajun9@126.com>
2020-04-15 19:50:25 +0800

    socket

--- net/socketUtils.h
+++ net/socketUtils.h
@@ -5,14 +5,41 @@
 #ifndef LINUXSERVER_SOCKETUTILS_H
 #define LINUXSERVER_SOCKETUTILS_H
 
+#include <bits/sockaddr.h>
+
 namespace ynet {
 
 class SocketUtils {
     NonCopyable(SocketUtils)
 public:
     static ssize_t read(int sockfd, void* buf, size_t count);
+    static ssize_t readv(int sockfd, const struct iovec* iov, int iovcnt);
     static ssize_t write(int sockfd, const void*buf, size_t count);
+    static void close(int sockfd);
+    static void shutdownWrite(int sockfd);
 
+    static int createNonblockingOrDie(sa_family_t family);
+    static int connect(int sockfd, const struct sockaddr* addr);
+    static void bindOrDie(int sockfd, const struct sockaddr* addr);
+    static void listenOrDie(int sockfd);
+    static int accept(int sockfd, struct sockaddr_in6* addr);
+
+    void toIpPort(char* buf, size_t size, const struct sockaddr* addr);
+    void toIp(char* buf, size_t size, const struct sockaddr* addr);
+    void fromIpPort(const char* ip, uint16_t port, struct sockaddr_in* addr);
+    void fromIpPort(const char* ip, uint16_t port, struct sockaddr_in6* addr);
+
+    int getSocketError(int sockfd);
+    struct sockaddr_in6 getLocalAddr(int sockfd);
+    struct sockaddr_in6 getPeerAddr(int sockfd);
+
+    bool isSelfConnect(int sockfd);
+
+    static const struct sockaddr* sockaddr_cast(const struct sockaddr_in* addr);
+    static const struct sockaddr* sockaddr_cast(const struct sockaddr_in6* addr);
+    static struct sockaddr* sockaddr_cast(struct sockaddr_in6* addr);
+    static const struct sockaddr_in* sockaddr_in_cast(const struct sockaddr* addr);
+    static const struct sockaddr_in6* sockaddr_in6_cast(const struct sockaddr* addr);
 };
 
 } //namespace ynet
